0ac7b1e4895181d696608c230bbfab79d29daa08
EskiNott <heisesd@gmail.com>
2023-03-27 17:24:30 +0800

    新插件 BattleSystem.js 更改 技能/防御/获得TP 10 => 0 更改敌人技能的TP消耗为0

---
+++ js/plugins/BattleSystem.js
@@ -0,0 +1,24 @@
+/*
+* 修改Tp值获取方式、上限算法
+* 
+*
+* */
+(() => {
+    //注释掉受伤影响Tp的功能 rmmz_objects.js/Line 3930
+    Game_Battler.prototype.onDamage = function(value) {
+        this.removeStatesByDamage();
+        //this.chargeTpByDamage(value / this.mhp);
+    };
+
+    //每局开始Tp清零 rmmz_objects.js/Line 3850
+    Game_Battler.prototype.initTp = function() {
+        this.setTp(1);
+    };
+
+    //修改回合结束时自动恢复TP为1 rmmz_objects.js/Line 3882
+    Game_Battler.prototype.regenerateTp = function() {
+        //const value = Math.floor(100 * this.trg);
+        //this.gainSilentTp(value);
+        this.gainSilentTp(1);
+    };
+})()
