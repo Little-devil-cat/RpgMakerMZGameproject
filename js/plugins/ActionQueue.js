7b84166ebbf84e6fbcf2c51d1913a673567949b4
Hale-Lin <HaleLin@outlook.com>
2023-03-30 22:12:54 +0800

    plugin: ActionQueue UI

--- js/plugins/ActionQueue.js
+++ js/plugins/ActionQueue.js
@@ -4,7 +4,35 @@
  *
  */
 
-BattleManager.startTurn = function() {
+// 实现文字条在行动顺序下方
+
+Scene_Battle.prototype.logWindowRect = function() {
+    const wx = Graphics.boxWidth / 4;
+    const wy = Graphics.boxHeight / 6;
+    const ww = Graphics.boxWidth / 2;
+    const wh = this.calcWindowHeight(10, false);
+    return new Rectangle(wx, wy, ww, wh);
+};
+
+Window_BattleLog.prototype.drawLineText = function(index) {
+    const rect = this.lineRect(index);
+    this.contents.clearRect(rect.x, rect.y, rect.width, rect.height);
+    // this.drawTextEx(this._lines[index], rect.x, rect.y, rect.width);
+    this.drawTextExCenter(this._lines[index], rect.x, rect.y, rect.width);
+};
+
+Window_Base.prototype.drawTextExCenter = function(text, x, y, width) {
+    // 计算文本宽度
+    const textWidth = this.drawTextEx(text, 0, this.contents.height);
+    // 计算左侧边距
+    const leftMargin = (this.width - textWidth) / 2;
+    // 输出文本
+    this.drawTextEx(text, x + leftMargin, y, width);
+};
+
+//=====================================================================================
+
+BattleManager.startTurn = function () {
     this._phase = "turn";
     $gameTroop.increaseTurn();
     $gameParty.requestMotionRefresh();
@@ -16,7 +44,7 @@ BattleManager.startTurn = function() {
     }
 };
 
-BattleManager.startBattle = function() {
+BattleManager.startBattle = function () {
     this._phase = "start";
     $gameSystem.onBattleStart();
     $gameParty.onBattleStart(this._preemptive);
@@ -24,14 +52,14 @@ BattleManager.startBattle = function() {
     this.makeActionOrders() // add
     this.displayStartMessages();
 };
-BattleManager.endTurn = function() {
+BattleManager.endTurn = function () {
     this._phase = "turnEnd";
     this._preemptive = false;
     this._surprise = false;
     this.makeActionOrders() // add
 };
 
-BattleManager.makeActionOrders = function() {
+BattleManager.makeActionOrders = function () {
     const battlers = [];
     if (!this._surprise) {
         battlers.push(...$gameParty.battleMembers());
@@ -62,9 +90,8 @@ function Window_Speed() {
 Window_Speed.prototype = Object.create(Window_Base.prototype);
 Window_Speed.prototype.constructor = Window_Speed;
 
-Window_Speed.prototype.initialize = function(rect) {
+Window_Speed.prototype.initialize = function (rect) {
     Window_Base.prototype.initialize.call(this, rect);
-    this._battlersNum = BattleManager._actionBattlers.length;
     this.refresh();
 };
 
@@ -82,7 +109,7 @@ Sprite_myEnemy.prototype = Object.create(Sprite.prototype);
 Sprite_myEnemy.prototype.constructor = Sprite_myEnemy;
 
 // 加载敌人图像
-Sprite_myEnemy.prototype.createMainBitmap = function() {
+Sprite_myEnemy.prototype.createMainBitmap = function () {
     var name = this._enemy.battlerName();
     var hue = this._enemy.battlerHue();
     this.bitmap = ImageManager.loadSvEnemy(name, hue);
@@ -102,13 +129,13 @@ Sprite_myActor.prototype = Object.create(Sprite.prototype);
 Sprite_myActor.prototype.constructor = Sprite_myActor;
 
 // 加载友军图像
-Sprite_myActor.prototype.createMainBitmap = function() {
+Sprite_myActor.prototype.createMainBitmap = function () {
     var name = this._actor._faceName;
     this.bitmap = ImageManager.loadFace(name);
 };
 
 
-Window_Speed.prototype.refresh = function() {
+Window_Speed.prototype.refresh = function () {
     if (this._innerChildren.length !== BattleManager._actionBattlers.length) {
         if (this._innerChildren.length > BattleManager._actionBattlers.length) {
             var children = this._innerChildren;
@@ -117,33 +144,34 @@ Window_Speed.prototype.refresh = function() {
             }
         }
 
-        let x = 50;
-        let y;
+        let scale = 0.4;
+        let spacingX = ImageManager.faceWidth * scale * 1.1
+        let x = (Graphics.boxWidth - spacingX * BattleManager._actionBattlers.length) / 2;
+        let y = this.innerHeight / 2;
         for (const actionBattler of BattleManager._actionBattlers) {
+            // 死了就不画了
+            if (actionBattler._states.indexOf(1) >= 0) {
+                continue
+            }
             if (actionBattler._actorId) {
-                y = 80;
                 let spriteActor = new Sprite_myActor(actionBattler)
+                spriteActor.setFrame((actionBattler._faceIndex % 4) * ImageManager.faceWidth, Math.floor(actionBattler._faceIndex / 4) * ImageManager.faceHeight, ImageManager.faceWidth, ImageManager.faceHeight)
+                spriteActor.scale.x = scale;
+                spriteActor.scale.y = scale;
                 spriteActor.x = x;
                 spriteActor.y = y;
-                spriteActor.setFrame(0,0,spriteActor.width / 4, spriteActor.height / 2)
-                spriteActor.scale.x = 0.2;
-                spriteActor.scale.y = 0.2;
                 this.addInnerChild(spriteActor);
-            }
-            else {
-                y = 30;
+            } else {
                 let spriteEnemy = new Sprite_myEnemy(actionBattler)
+                spriteEnemy.setFrame(0, 0, ImageManager.faceWidth, ImageManager.faceHeight);
+                spriteEnemy.scale.x = scale;
+                spriteEnemy.scale.y = scale;
                 spriteEnemy.x = x;
                 spriteEnemy.y = y;
-                spriteEnemy.scale.x = 0.2;
-                spriteEnemy.scale.y = 0.2;
                 this.addInnerChild(spriteEnemy);
             }
             console.log(BattleManager._actionBattlers, this._innerChildren)
-            // if (actionBattler._states.indexOf(1) > 0) {
-            //     this.drawText("dead", x, y + 10, this.contentsWidth(), 'center');
-            // }
-            x += 50;
+            x += spacingX;
         }
     }
 
@@ -151,7 +179,7 @@ Window_Speed.prototype.refresh = function() {
 
 Scene_Battle._speedWindow = null
 
-Scene_Battle.prototype.createAllWindows = function() {
+Scene_Battle.prototype.createAllWindows = function () {
     this.createLogWindow();
     this.createStatusWindow();
     this.createPartyCommandWindow();
@@ -165,9 +193,10 @@ Scene_Battle.prototype.createAllWindows = function() {
     Scene_Message.prototype.createAllWindows.call(this);
 };
 
-Scene_Battle.prototype.createSpeedWindow = function() {
-    this._speedWindow = new Window_Speed(new Rectangle( Graphics.boxWidth * 2 / 3, 10, Graphics.boxWidth / 3, Graphics.boxHeight / 5));
+Scene_Battle.prototype.createSpeedWindow = function () {
+    this._speedWindow = new Window_Speed(new Rectangle(0, 0, Graphics.boxWidth, Graphics.boxHeight / 6));
     this.addWindow(this._speedWindow);
+    console.log(this._speedWindow)
 };
 
 var old_Scene_Battle_update = Scene_Battle.prototype.update
